03c81117c8d26da4bb2e5bcff7931f46257f4cc9
Ituking <77528963+Ituking@users.noreply.github.com>
2024-05-14 12:01:35 +0900

    Add network and repository configurations for improved data management

---
+++ lib/repositories/weather_repository_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app/repositories/weather_repository.dart';
+import 'package:weather_app/services/weather_api_client_provider.dart';
+
+// WeatherRepositoryProviderは、WeatherApiClientに基づいてWeatherRepositoryのインスタンスを作成します。
+// このProviderは、WeatherApiClientのインスタンスを取得し、それを使ってWeatherRepositoryを初期化します。
+// WeatherRepositoryはAPIから天気データを取得するためのメソッドを提供し、これをViewModel層で使用します。
+final weatherRepositoryProvider = Provider<WeatherRepository>((ref) {
+  // WeatherApiClientProviderからWeatherApiClientのインスタンスを取得。
+  // このインスタンスはAPI呼び出しを担当し、その結果をRepository層で利用可能にします。
+  final weatherApiClient = ref.watch(weatherApiClientProvider);
+  // WeatherApiClientを使用して、天気データの取得と操作を行うWeatherRepositoryを生成。
+  return WeatherRepository(apiClient: weatherApiClient);
+});
