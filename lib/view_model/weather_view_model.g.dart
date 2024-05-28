e98a97380a8b6f40ce7244adeea8f1dd869a4f9f
Ituking <77528963+Ituking@users.noreply.github.com>
2024-05-28 16:17:36 +0900

    Add WeatherViewModel, providers, and tests

---
+++ lib/view_model/weather_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherViewModelHash() => r'c9dc5ec993d954c98e7e31b237815ce04b72bda9';
+
+/// See also [WeatherViewModel].
+@ProviderFor(WeatherViewModel)
+final weatherViewModelProvider = AutoDisposeNotifierProvider<WeatherViewModel,
+    WeatherViewModelState>.internal(
+  WeatherViewModel.new,
+  name: r'weatherViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WeatherViewModel = AutoDisposeNotifier<WeatherViewModelState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
