27219916ed7eb7989f551390a623b1e85eb36180
Ituking <77528963+Ituking@users.noreply.github.com>
2024-05-28 14:21:56 +0900

    Add generated mock file for CitySearchViewModel

---
+++ test/mocks/mock_city_search_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:weather_app/view_model/city_search_view_model.dart';
+
+// @GenerateMocksアノテーションを使用して、モッククラスを生成します。
+// CitySearchViewModelクラスのモックを生成対象とします。
+@GenerateMocks([CitySearchViewModel])
+void main() {}
