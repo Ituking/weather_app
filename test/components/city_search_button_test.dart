0d595a2b4aa4e48968f2ad5c3c5fc5be94beab8c
Ituking <77528963+Ituking@users.noreply.github.com>
2024-06-18 06:31:19 +0900

    Refactor: Update CitySearchButton Tests with CustomMockCitySearchViewModel

--- test/components/city_search_button_test.dart
+++ test/components/city_search_button_test.dart
@@ -5,26 +5,26 @@ import 'package:mockito/mockito.dart';
 import 'package:weather_app/components/city_search_button.dart';
 import 'package:weather_app/view_model/city_search_state.dart';
 import 'package:weather_app/view_model/providers/city_search_view_model_provider.dart';
-
-import '../mocks/mock_city_search_view_model.mocks.dart';
+import '../mocks/custom_mock_city_search_view_model.dart';
+import '../view_model/providers/custom_mock_city_search_view_model_provider.dart';
 
 void main() {
-  group('CitySearchButton Tests', () {
-    late MockCitySearchViewModel mockViewModel;
+  group('CitySearchButtonのテスト', () {
+    late CustomMockCitySearchViewModel mockViewModel;
     late ProviderContainer container;
 
     setUp(() {
       // モックのViewModelを初期化
-      mockViewModel = MockCitySearchViewModel();
+      mockViewModel = CustomMockCitySearchViewModel();
       // ProviderContainerの初期化
       container = ProviderContainer(overrides: [
-        citySearchViewModelProvider.overrideWith(() => mockViewModel),
+        customMockCitySearchViewModelProvider.overrideWithValue(mockViewModel),
       ]);
     });
 
-    testWidgets('CitySearchButton is displayed', (WidgetTester tester) async {
+    testWidgets('CitySearchButtonが表示される', (WidgetTester tester) async {
       // ViewModelの状態を設定
-      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: false));
+      mockViewModel.setState(CitySearchState(isLoading: false));
 
       // テスト対象のウィジェットを構築
       await tester.pumpWidget(UncontrolledProviderScope(
@@ -40,10 +40,10 @@ void main() {
       expect(find.byType(ElevatedButton), findsOneWidget);
     });
 
-    testWidgets('displays CircularProgressIndicator when isLoading is true',
+    testWidgets('初期状態で"Search"が表示され、CircularProgressIndicatorが表示されない',
         (WidgetTester tester) async {
-      // ViewModelの状態を設定（ローディング中）
-      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: true));
+      // 初期状態設定
+      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: false));
 
       // テスト対象のウィジェットを構築
       await tester.pumpWidget(UncontrolledProviderScope(
@@ -54,75 +54,40 @@ void main() {
           ),
         ),
       ));
+
+      // 初期状態で"Search"テキストが表示されていることを確認
+      expect(find.text('Search'), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+
+    testWidgets('ローディング状態に遷移するとCircularProgressIndicatorが表示される',
+        (WidgetTester tester) async {
+      // 初期状態設定
+      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: false));
+
+      // テスト対象のウィジェットを構築
+      await tester.pumpWidget(UncontrolledProviderScope(
+        container: container,
+        child: const MaterialApp(
+          home: Scaffold(
+            body: CitySearchButton(),
+          ),
+        ),
+      ));
+
+      // 初期状態で"Search"テキストが表示されていることを確認
+      expect(find.text('Search'), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+
+      // 状態をローディング中に変更
+      container
+          .read(citySearchViewModelProvider.notifier)
+          .setState(CitySearchState(isLoading: true));
+      await tester.pump();
 
       // CircularProgressIndicatorが表示されていることを確認
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
-    });
-
-    testWidgets(
-        'displays "Search" text and is clickable when isLoading is false',
-        (WidgetTester tester) async {
-      // ViewModelの状態を設定（ローディング中ではない）
-      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: false));
-
-      // テスト対象のウィジェットを構築
-      await tester.pumpWidget(UncontrolledProviderScope(
-        container: container,
-        child: const MaterialApp(
-          home: Scaffold(
-            body: CitySearchButton(),
-          ),
-        ),
-      ));
-
-      // ElevatedButtonが表示されていることを確認
-      expect(find.byType(ElevatedButton), findsOneWidget);
-      // "Search"テキストが表示されていることを確認
-      expect(find.text('Search'), findsOneWidget);
-
-      // ボタンをタップ可能か確認し、fetchWeatherメソッドが呼び出されることを確認
-      await tester.tap(find.byType(ElevatedButton));
-      verify(mockViewModel.fetchWeather()).called(1);
-    });
-
-    testWidgets('Button is disabled when isLoading is true',
-        (WidgetTester tester) async {
-      // ViewModelの状態を設定（ローディング中）
-      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: true));
-
-      // テスト対象のウィジェットを構築
-      await tester.pumpWidget(UncontrolledProviderScope(
-        container: container,
-        child: const MaterialApp(
-          home: Scaffold(
-            body: CitySearchButton(),
-          ),
-        ),
-      ));
-
-      // ElevatedButtonが無効化されていることを確認
-      final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
-      expect(button.onPressed, isNull);
-    });
-
-    testWidgets('Button is enabled when isLoading is false',
-        (WidgetTester tester) async {
-      // ViewModelの状態を設定（ローディング中ではない）
-      when(mockViewModel.state).thenReturn(CitySearchState(isLoading: false));
-
-      // テスト対象のウィジェットを構築
-      await tester.pumpWidget(UncontrolledProviderScope(
-        container: container,
-        child: const MaterialApp(
-          home: Scaffold(
-            body: CitySearchButton(),
-          ),
-        ),
-      ));
-
-      // ElevatedButtonが有効化されていることを確認
-      final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
-      expect(button.onPressed, isNotNull);
+      expect(find.text('Search'), findsNothing);
     });
   });
 }
