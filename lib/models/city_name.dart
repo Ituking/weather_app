c5d1306248bdca25b4f7500766bff58978117198
Ituking <77528963+Ituking@users.noreply.github.com>
2025-01-19 20:51:44 +0900

    Refactor: Add Dartdoc comments to model classes for clarity
    - Added Dartdoc comments to CityName, WeatherDescription, WeatherMain, WeatherModel, and WeatherWind classes.
    - Documented constructors, fields, and factory methods for better understanding and maintainability.
    - Replaced inline comments with structured Dartdoc comments to ensure consistency across all model classes.
    - Improved readability and adherence to Dart documentation standards.

--- lib/models/city_name.dart
+++ lib/models/city_name.dart
@@ -4,15 +4,16 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'city_name.freezed.dart';
 part 'city_name.g.dart';
 
-// `CityName` クラスは、都市の名前を表します。
+/// [CityName]クラスは、都市の名前を表します。
 @freezed
 class CityName with _$CityName {
-  // コンストラクタ。都市名 [name] を受け取ります。
+  /// コンストラクタ。都市名[name]を受け取ります。
   factory CityName({
-    required String name, // 都市の名前を表します。
+    /// 都市の名前を表します。
+    required String name,
   }) = _CityName;
 
-  // JSON から `CityName` インスタンスを生成するファクトリメソッド。
+  /// JSONから[CityName]インスタンスを生成するファクトリメソッド。
   factory CityName.fromJson(Map<String, dynamic> json) =>
       _$CityNameFromJson(json);
 }
--- lib/models/weather_wind.dart
+++ lib/models/weather_wind.dart
@@ -3,15 +3,16 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'weather_wind.freezed.dart';
 part 'weather_wind.g.dart';
 
-// `WeatherWind` クラスは、風の情報を表します。
+/// [WeatherWind]クラスは、風の情報を表します。
 @freezed
 class WeatherWind with _$WeatherWind {
-  // コンストラクタ。風速 [speed] を受け取ります。
+  /// コンストラクタ。風速[speed]を受け取ります。
   factory WeatherWind({
-    required double speed, // 風速を表します。
+    /// 風速を表します。
+    required double speed,
   }) = _WeatherWind;
 
-  // JSON から `WeatherWind` インスタンスを生成するファクトリメソッド。
+  /// JSONから[WeatherWind]インスタンスを生成するファクトリメソッド。
   factory WeatherWind.fromJson(Map<String, dynamic> json) =>
       _$WeatherWindFromJson(json);
 }
