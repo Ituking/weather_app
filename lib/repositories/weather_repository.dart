be0d2dc5f7f1312f380551184845363f469e15af
Ituking <77528963+Ituking@users.noreply.github.com>
2025-01-19 21:01:32 +0900

    Refactor: Add Dartdoc comments to WeatherRepository and related providers
    - Improved documentation for the WeatherRepository interface to clarify its purpose and usage.
    - Enhanced Dartdoc comments for weatherRepositoryProvider to explain its role in providing a WeatherRepository instance.
    - Replaced inline comments with structured Dartdoc comments for consistency.
    - Simplified and formatted the provider initialization for better readability.

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -1,14 +1,10 @@
 import '../core/network/response/result.dart';
 import '../core/network/response/weather_response.dart';
 
-// WeatherRepositoryインターフェースは、天気データを取得するための契約を定義します。
-// 具体的な実装クラスは、このインターフェースを実装し、天気データの取得ロジックを提供します。
+/// 天気データを取得するためのインターフェース。
 abstract class WeatherRepository {
-  // 指定された都市名 [cityName] の天気データを取得します。
-  // 成功した場合は [WeatherResponse] のインスタンスを返します。
-  // データ取得または変換に失敗した場合は例外をスローします。
-  //
-  // [cityName] - 天気データを取得する都市の名前
-  // 戻り値 - 取得した天気データを含む Future<Result<WeatherResponse>>
+  /// 指定された都市名の天気データを取得します。
+  ///
+  /// [cityName] : 天気データを取得する都市の名前。
   Future<Result<WeatherResponse>> getWeather(String cityName);
 }
