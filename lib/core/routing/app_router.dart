990a8341dfb99675fc627d43312e7d16d3379261
Ituking <77528963+Ituking@users.noreply.github.com>
2025-01-19 20:42:11 +0900

    Refactor: Improve Dartdoc comments and readability in AppRouter
    - Added detailed Dartdoc comments for routes and their purposes.
    - Enhanced code structure and readability in the routing configuration.
    - Clarified the purpose of each route, including CitySearchScreen, WeatherResultScreen, and ErrorDisplayScreen.
    - Replaced inline comments with structured Dartdoc comments for consistency.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -7,37 +7,42 @@ import '../../view/screens/weather_result_screen.dart';
 import '../../view_model/error_view_model.dart';
 import '../../view_model/providers/error_view_model_provider.dart';
 
+/// アプリケーションのルーターを定義するクラス。
 class AppRouter {
-  // アプリケーションのルーターを定義
-  static final router = GoRouter(routes: [
-    // ルート設定: ルートパス '/' に対応する画面を CitySearchScreen に設定
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const CitySearchScreen(),
-    ),
-    // ルート設定: ルートパス '/result' に対応する画面を WeatherResultScreen に設定
-    GoRoute(
-      path: '/result',
-      builder: (context, state) => const WeatherResultScreen(),
-    ),
-    // ルート設定: エラー画面に遷移するルートを定義
-    GoRoute(
-      path: '/error',
-      builder: (context, state) {
-        // エラーメッセージをクエリパラメータから取得して渡す
-        final errorMessage = state.extra.toString();
+  /// アプリケーションのルーター設定。
+  static final router = GoRouter(
+    routes: [
+      /// ルート設定: ルートパス '/' に対応する画面を[CitySearchScreen]に設定。
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const CitySearchScreen(),
+      ),
 
-        return ProviderScope(
-          overrides: [
-            errorViewModelProvider.overrideWith(() {
-              final viewModel = ErrorViewModel();
-              viewModel.setErrorMessage(errorMessage);
-              return viewModel;
-            }),
-          ],
-          child: const ErrorDisplayScreen(),
-        );
-      },
-    ),
-  ]);
+      /// ルート設定: ルートパス '/result' に対応する画面を[WeatherResultScreen]に設定。
+      GoRoute(
+        path: '/result',
+        builder: (context, state) => const WeatherResultScreen(),
+      ),
+
+      /// ルート設定: エラー画面に遷移するルートを定義。
+      GoRoute(
+        path: '/error',
+        builder: (context, state) {
+          // エラーメッセージをクエリパラメータから取得して渡す
+          final errorMessage = state.extra.toString();
+
+          return ProviderScope(
+            overrides: [
+              errorViewModelProvider.overrideWith(() {
+                final viewModel = ErrorViewModel();
+                viewModel.setErrorMessage(errorMessage);
+                return viewModel;
+              }),
+            ],
+            child: const ErrorDisplayScreen(),
+          );
+        },
+      ),
+    ],
+  );
 }
