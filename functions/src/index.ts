cd9a9f0ca947a9f6cdbdc41843523c64600dd138
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-03 01:40:12 +0900

    refactor: clean up and streamline exports in index.ts
    - Enforce double quotes for consistency with ESLint rules
    - Replace individual exports with wildcard exports
    - Remove redundant import statements

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,6 @@
-import * as admin from 'firebase-admin';
-
-import { getWeatherForCity } from './weather_controller';
-
+import * as admin from "firebase-admin";
 admin.initializeApp();
 
-export { getWeatherForCity };
+export * from "./weather_controller";
+export * from "./weather_service";
 
-export * from './weather_controller';
-export * from './weather_service';
