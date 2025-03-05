10c1e5f9d12a3f570ab08d5d63df3d4fd829fedb
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-05 18:45:00 +0900

    test: add Firebase mock setup for CitySearchInput tests
    - Initialize Firebase using setupFirebaseMocks() in setUpAll()
    - Ensure Firebase.initializeApp() is called before tests run
    - Prevent Firebase-related errors in widget tests

--- test/components/city_search_input_test.dart
+++ test/components/city_search_input_test.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -5,7 +6,14 @@ import 'package:weather_app/components/city_search_input.dart';
 import 'package:weather_app/core/strings/city_name_validator_strings.dart';
 import 'package:weather_app/core/strings/city_search_input_strings.dart';
 
+import '../mocks/mock_firebase.dart';
+
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupFirebaseMocks();
+    await Firebase.initializeApp();
+  });
   group('CitySearchInputのテスト', () {
     testWidgets('CitySearchInputが表示される', (WidgetTester tester) async {
       // テストウィジェットを構築
