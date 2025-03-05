45d5660c365ca619cedb2454989226587f2c6021
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-05 19:02:06 +0900

    test: add Firebase mock setup for widget tests
    - Initialize Firebase using setupFirebaseMocks() in setUpAll()
    - Ensure Firebase.initializeApp() is called before running tests
    - Prevent Firebase-related errors in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app/main.dart';
 import 'package:weather_app/view/screens/city_search_screen.dart';
 
+import 'mocks/mock_firebase.dart';
+
 void main() {
+  setUpAll(() async {
+    setupFirebaseMocks();
+    await Firebase.initializeApp();
+  });
   testWidgets('CitySearchScreen displays correctly',
       (WidgetTester tester) async {
     // テストウィジェットをProviderScopeでラップ
