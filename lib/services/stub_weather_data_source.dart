375a0a1789801038c65151f1d06950b3635c7dc0
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-20 01:18:33 +0900

    refactor: update StubWeatherDataSource to align with new WeatherResponse structure
    - Removed WeatherList dependency and replaced it with direct WeatherMain, WeatherDescription, and WeatherWind properties.
    - Updated sample weather data to match the updated WeatherResponse model.
    - Improved data structure consistency and simplified response creation logic.

--- lib/services/stub_weather_data_source.dart
+++ lib/services/stub_weather_data_source.dart
@@ -4,7 +4,6 @@ import '../core/network/api_error.dart';
 import '../core/network/response/weather_response.dart';
 import '../models/city_name.dart';
 import '../models/weather_description.dart';
-import '../models/weather_list.dart';
 import '../models/weather_main.dart';
 import '../models/weather_wind.dart';
 import 'i_weather_api_client.dart';
@@ -25,18 +24,13 @@ class StubWeatherDataSource implements IWeatherApiClient {
 
     // 有効な都市名が指定された場合、固定された天気データを返します。
     return Result.success(WeatherResponse(
-      list: [
-        WeatherList(
-          main: WeatherMain(temp: 20.0, humidity: 70), // 気温と湿度
-          weather: [
-            WeatherDescription(
-              description: 'Sunny',
-              icon: '01d',
-            )
-          ], // 天気の説明
-          wind: WeatherWind(speed: 5.0), // 風速
-        )
-      ],
+      main: WeatherMain(temp: 20.0, humidity: 70), // 気温と湿度
+      weather: WeatherDescription(
+        description: 'Sunny',
+        icon: '01d',
+      ),
+      // 天気の説明
+      wind: WeatherWind(speed: 5.0), // 風速
       city: CityName(name: cityName), // 都市名
     ));
   }
