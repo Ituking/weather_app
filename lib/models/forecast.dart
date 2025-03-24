94499fdc986c6f51ee58e3c205071002d5f565ed
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-24 23:58:59 +0900

    feat: Add Forecast model with JSON serialization
    Added a new Forecast model to represent weather forecast data.
    - Implemented using freezed for immutable data classes.
    - Supports JSON serialization with json_serializable.
    - Includes default values for safer data handling.
    This model will improve data management and enhance flexibility when working with weather forecast information.

---
+++ lib/models/forecast.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'forecast.freezed.dart';
+part 'forecast.g.dart';
+
+@freezed
+class Forecast with _$Forecast {
+  const factory Forecast({
+    @Default('') String id,
+    @Default('') String city,
+    @Default('') String description,
+    @Default(0.0) double humidity,
+    @Default(0.0) double temperature,
+    @Default(0.0) double windSpeed,
+  }) = _Forecast;
+
+  const Forecast._();
+
+  factory Forecast.fromJson(Map<String, dynamic> json) =>
+      _$ForecastFromJson(json);
+}
