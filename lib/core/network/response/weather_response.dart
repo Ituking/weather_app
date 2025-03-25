19fe1d2a26a6b4e9223a3ba87286c99f77d0a05f
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-26 00:06:27 +0900

    refactor: Remove WeatherResponse in favor of Forecast model
    - Deprecated and removed WeatherResponse class, including its implementation and associated generated files.
    - Updated the codebase to rely solely on the Forecast model for weather data representation.
    - Improved data structure consistency by consolidating the weather response logic under the Forecast model.
    
    This change simplifies the data model and aligns with the new architecture.

--- lib/core/network/response/weather_response.dart
+++ lib/core/network/response/weather_response.dart
@@ -1,32 +1,19 @@
-import 'package:flutter/foundation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:weather_app/models/converters/city_name_converter.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../../models/city_name.dart';
-import '../../../models/converters/weather_description_converter.dart';
-import '../../../models/converters/weather_main_converter.dart';
-import '../../../models/converters/weather_wind_converter.dart';
-import '../../../models/weather_description.dart';
-import '../../../models/weather_main.dart';
-import '../../../models/weather_wind.dart';
-import 'converters/weather_response_converter.dart';
+// import '../../../models/forecast.dart';
 
-part 'weather_response.freezed.dart';
-part 'weather_response.g.dart';
+// part 'weather_response.freezed.dart';
+// part 'weather_response.g.dart';
 
-/// [WeatherResponse] クラスは、天気情報のレスポンス全体を表します。
-@freezed
-class WeatherResponse with _$WeatherResponse {
-  /// コンストラクタ。
-  @WeatherResponseConverter()
-  factory WeatherResponse({
-    @WeatherMainConverter() required WeatherMain main,
-    @WeatherDescriptionConverter() required WeatherDescription weather,
-    @WeatherWindConverter() required WeatherWind wind,
-    @CityNameConverter() required CityName city,
-  }) = _WeatherResponse;
+// /// [WeatherResponse] クラスは、天気情報のレスポンス全体を表します。
+// @freezed
+// class WeatherResponse with _$WeatherResponse {
+//   factory WeatherResponse({
+//     required Forecast forecast,
+//   }) = _WeatherResponse;
 
-  /// JSON から [WeatherResponse] インスタンスを生成
-  factory WeatherResponse.fromJson(Map<String, dynamic> json) =>
-      _$WeatherResponseFromJson(json);
-}
+//   /// JSON から [WeatherResponse] インスタンスを生成
+//   factory WeatherResponse.fromJson(Map<String, dynamic> json) =>
+//       _$WeatherResponseFromJson(json);
+// }
