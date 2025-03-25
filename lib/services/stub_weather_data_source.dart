0adb5b3d1b8f01ebb4e1b67f1b713bad63238478
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-26 00:29:15 +0900

    refactor: Replace WeatherResponse with Forecast in StubWeatherDataSource
    - Updated StubWeatherDataSource to return Forecast instead of WeatherResponse.
    - Adjusted data model to align with Forecast structure, including:
    - Added id field for consistent data identification.
    - Replaced main, weather, and wind fields with temperature, humidity, and windSpeed.
    - Removed unused imports related to the previous WeatherResponse model.
    
    This change ensures StubWeatherDataSource is compatible with the new Forecast model, improving code clarity and consistency.

--- lib/services/stub_weather_data_source.dart
+++ lib/services/stub_weather_data_source.dart
@@ -1,11 +1,6 @@
-import 'package:weather_app/core/network/response/result.dart';
-
 import '../core/network/api_error.dart';
-import '../core/network/response/weather_response.dart';
-import '../models/city_name.dart';
-import '../models/weather_description.dart';
-import '../models/weather_main.dart';
-import '../models/weather_wind.dart';
+import '../core/network/response/result.dart';
+import '../models/forecast.dart';
 import 'i_weather_api_client.dart';
 
 /// [StubWeatherDataSource]クラスは、[IWeatherApiClient]のスタブ実装です。
@@ -13,7 +8,7 @@ import 'i_weather_api_client.dart';
 /// テスト目的で使用され、固定されたデータを返します。
 class StubWeatherDataSource implements IWeatherApiClient {
   @override
-  Future<Result<WeatherResponse>> fetchWeather(String cityName) async {
+  Future<Result<Forecast>> fetchWeather(String cityName) async {
     // 無効な都市名が指定された場合、エラーレスポンスを返す
     if (cityName == 'InvalidCity') {
       return Result.failure(ApiError(
@@ -23,15 +18,13 @@ class StubWeatherDataSource implements IWeatherApiClient {
     }
 
     // 有効な都市名が指定された場合、固定された天気データを返します。
-    return Result.success(WeatherResponse(
-      main: WeatherMain(temp: 20.0, humidity: 70), // 気温と湿度
-      weather: WeatherDescription(
-        description: 'Sunny',
-        icon: '01d',
-      ),
-      // 天気の説明
-      wind: WeatherWind(speed: 5.0), // 風速
-      city: CityName(name: cityName), // 都市名
+    return Result.success(Forecast(
+      id: 'stub_id_123',
+      city: cityName, // 都市名
+      description: 'Sunny', // 天気の説明
+      temperature: 20.0, // 気温
+      humidity: 70.0, // 湿度
+      windSpeed: 5.0, // 風速
     ));
   }
 }
