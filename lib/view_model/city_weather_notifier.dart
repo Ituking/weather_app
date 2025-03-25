d02ef1ac220c7d23995e582a3871b4e8cb5399a3
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-26 00:34:47 +0900

    refactor: Update CityWeatherNotifier to use Forecast model
    - Replaced WeatherResponse with Forecast in CityWeatherNotifier.
    - Updated provider definition to reflect the new Forecast data model.
    - Ensured compatibility with the updated repository structure.
    
    This change aligns the CityWeatherNotifier with the revised data model to improve data consistency and maintainability.

--- lib/view_model/city_weather_notifier.dart
+++ lib/view_model/city_weather_notifier.dart
@@ -5,19 +5,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/logger/logger_provider.dart';
 import '../core/network/api_error.dart';
 import '../core/network/response/result.dart';
-import '../core/network/response/weather_response.dart';
+import '../models/forecast.dart';
 import '../repositories/weather_repository.dart';
 import '../repositories/weather_repository_provider.dart';
 import 'providers/dio_error_handler_provider.dart';
 
 /// [CityWeatherNotifier]は、指定された都市の天気情報を非同期に取得し、
 /// その結果を管理するための[Notifier]クラスです。
-class CityWeatherNotifier
-    extends Notifier<AsyncValue<Result<WeatherResponse>>> {
+class CityWeatherNotifier extends Notifier<AsyncValue<Result<Forecast>>> {
   late final WeatherRepository _weatherRepository;
 
   @override
-  AsyncValue<Result<WeatherResponse>> build() {
+  AsyncValue<Result<Forecast>> build() {
     _weatherRepository = ref.read(weatherRepositoryProvider);
     return const AsyncLoading();
   }
