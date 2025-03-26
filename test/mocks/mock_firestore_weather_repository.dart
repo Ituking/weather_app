dc5c1a043b87cf66975a93b4582381aeb153b6d5
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-26 21:58:10 +0900

    test: Add mock for FirestoreWeatherRepository using Mockito
    - Introduced mock_firestore_weather_repository.dart for generating mocks
    - Added corresponding generated mock file for use in unit tests

---
+++ test/mocks/mock_firestore_weather_repository.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:weather_app/repositories/firestore_weather_repository.dart';
+
+@GenerateMocks([FirestoreWeatherRepository])
+void main() {}
