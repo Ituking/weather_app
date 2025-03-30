fe37857a0dc76d4fde4c00fb101236af530abc18
Ituking <77528963+Ituking@users.noreply.github.com>
2025-03-30 16:17:32 +0900

    chore: remove DioErrorHandler and its provider after Dio deprecation
    - Deleted dio_error_handler.dart responsible for DioException to ApiError mapping
    - Removed dio_error_handler_provider.dart used for injecting DioErrorHandler
    - These are no longer needed after migrating API calls to Firebase Cloud Functions

--- lib/view_model/providers/dio_error_handler_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../core/network/dio_error_handler.dart';
-
-/// [DioErrorHandler]を提供する[Provider]
-final dioErrorHandlerProvider = Provider<DioErrorHandler>((ref) {
-  return DioErrorHandler();
-});
