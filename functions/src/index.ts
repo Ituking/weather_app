69ca1970527cc3029aa9b7d5734a82cfdc0d805d
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-07 14:27:46 +0900

    chore: export news controller and service from entry point
    - Prevent multiple Firebase admin initializations
    - Make getNewsForCity and related services accessible from index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,11 @@
 import * as admin from "firebase-admin";
-admin.initializeApp();
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 export * from "./weather_controller";
 export * from "./weather_service";
+
+export * from "./news_controller";
+export * from "./news_service";
 
