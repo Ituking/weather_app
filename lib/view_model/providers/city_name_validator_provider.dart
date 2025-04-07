ccaca58bdb00eacac52b7ba0421fa6f22dd2b135
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-07 18:44:28 +0900

    refactor(weather): Update CityNameValidator import path in provider
    - Updated import path in city_name_validator_provider.dart to reflect
    relocation of CityNameValidator to features/weather/validator/.

--- lib/view_model/providers/city_name_validator_provider.dart
+++ lib/view_model/providers/city_name_validator_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../validator/city_name_validator.dart';
+import '../../features/weather/validator/city_name_validator.dart';
 
 /// [CityNameValidator]を提供する[Provider]
 final cityNameValidatorProvider = Provider((ref) => CityNameValidator());
