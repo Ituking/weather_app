7dd7574041933286b197f19c7e98b0b6edd46794
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-17 23:28:03 +0900

    refactor(test): Update widget test import to reflect new UI path
    - Changed import from features/weather/... to ui/weather/screens/city_search_screen.dart
    - Ensures test aligns with updated project structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:weather_app/features/weather/view/screens/city_search_screen.dart';
 import 'package:weather_app/main.dart';
+import 'package:weather_app/ui/weather/screens/city_search_screen.dart';
 
 import 'features/weather/mocks/mock_firebase.dart';
 
