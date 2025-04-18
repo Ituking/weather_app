d9e6e901723dfcc275c7133be1e97c4072bfda3a
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-18 20:16:12 +0900

    test(ui/weather/widgets): Move and update widget tests with adjusted imports

--- test/ui/weather/widgets/city_search_input_test.dart
+++ test/ui/weather/widgets/city_search_input_test.dart
@@ -6,7 +6,7 @@ import 'package:weather_app/core/strings/city_name_validator_strings.dart';
 import 'package:weather_app/core/strings/city_search_input_strings.dart';
 import 'package:weather_app/ui/weather/widgets/city_search_input.dart';
 
-import '../mocks/mock_firebase.dart';
+import '../../../features/weather/mocks/mock_firebase.dart';
 
 void main() {
   setUpAll(() async {
