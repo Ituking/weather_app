d4a8cac09135c3a5df3361ba944dd3ca6b83a339
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-18 17:19:57 +0900

    chore(test): Clean up mock_weather_api_client main method comments

--- test/features/weather/mocks/mock_weather_api_client.dart
+++ test/features/weather/mocks/mock_weather_api_client.dart
@@ -1,9 +1,5 @@
 import 'package:mockito/annotations.dart';
 import 'package:weather_app/data/services/weather/weather_api_client.dart';
 
-// WeatherApiClientクラスのモックを生成するためのアノテーション。
-// `build_runner`コマンドを実行することで、指定されたクラスのモッククラスが自動的に生成されます。
 @GenerateMocks([WeatherApiClient])
-void main() {
-  // このファイルはモックを生成するためだけに存在します。`main` 関数は何も実行しません。
-}
+void main() {}
