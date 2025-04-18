32ee54ab6e25f119da074fdb6c3ead406c090a8b
Ituking <77528963+Ituking@users.noreply.github.com>
2025-04-18 22:56:13 +0900

    chore(test): update mock import path in widget_test.dart
    - Updated import to reflect new mock file location under test_helpers/mocks.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app/main.dart';
 import 'package:weather_app/ui/weather/screens/city_search_screen.dart';
 
-import 'features/weather/mocks/mock_firebase.dart';
+import 'test_helpers/mocks/mock_firebase.dart';
 
 void main() {
   setUpAll(() async {
