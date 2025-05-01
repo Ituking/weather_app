0f0ec65c87205db3aaa224487e8845c6a39d22a2
Ituking <77528963+Ituking@users.noreply.github.com>
2025-05-01 23:40:29 +0900

    refactor(api): change IWeatherApiClient to return raw Map instead of Forecast
    - Updated interface method to return Result<Map<String, dynamic>>
    - Removed dependency on Forecast model in API layer

--- lib/data/services/weather/i_weather_api_client.dart
+++ lib/data/services/weather/i_weather_api_client.dart
@@ -1,8 +1,7 @@
 import '../../../../core/network/response/result.dart';
-import '../../../../domain/models/weather/forecast.dart';
 
 /// 天気データを取得するAPIクライアントのインターフェース
 abstract class IWeatherApiClient {
   /// 指定された都市の天気情報を取得する
-  Future<Result<Forecast>> fetchWeather(String cityName);
+  Future<Result<Map<String, dynamic>>> fetchWeather(String cityName);
 }
