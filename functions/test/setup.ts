fd8e49b2c1d626c7ae53609292de29cd46490b2e
Ituking <77528963+Ituking@users.noreply.github.com>
2025-05-27 00:00:29 +0900

    chore(test): add chai-as-promised and export common expect utility
    - Configured chai to use chai-as-promised for async assertions
    - Exported expect from setup.ts for consistent usage in tests

--- functions/test/setup.ts
+++ functions/test/setup.ts
@@ -1,6 +1,12 @@
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 import * as dotenv from "dotenv";
 import * as admin from "firebase-admin";
 import firebaseFunctionsTest from "firebase-functions-test";
+
+chai.use(chaiAsPromised);
+
+export const expect = chai.expect;
 
 dotenv.config();
 const test = firebaseFunctionsTest();
